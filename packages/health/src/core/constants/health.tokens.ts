9227354889ab1ebe6f7e4bfb2d2e98eca057ac0b
Pixiedia, Inc. <pixiedia@gmail.com>
2026-09-02 19:41:58 +0300

    feat(health): add core tokens

---
+++ packages/health/src/core/constants/health.tokens.ts
@@ -0,0 +1,3 @@
+export const HEALTH_SERVICE = Symbol.for("figentra.health.service");
+export const HEALTH_REGISTRY = Symbol.for("figentra.health.registry");
+export const HEALTH_RUNTIME = Symbol.for("figentra.health.runtime");
