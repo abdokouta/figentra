1a2d6d7c0a725294cefe91fe04166b01f4d527e9
Pixiedia, Inc. <pixiedia@gmail.com>
2026-09-02 19:43:18 +0300

    feat(health): expose Nest adapter

---
+++ packages/health/src/nest/index.ts
@@ -0,0 +1,4 @@
+export * from "./health.module.js";
+export * from "./controllers/health.controller.js";
+export * from "./discovery/health-indicator.decorator.js";
+export * from "./discovery/indicator-loader.service.js";
