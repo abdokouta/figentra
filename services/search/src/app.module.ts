7073b95a7bb2e358e191495bac9b3665156a956b
Pixiedia, Inc. <pixiedia@gmail.com>
2026-09-08 19:42:05 +0300

    feat(search): add app module

---
+++ services/search/src/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { SearchController } from './modules/search/search.controller';
+import { SearchService } from './modules/search/search.service';
+
+@Module({
+  controllers: [SearchController],
+  providers: [SearchService],
+})
+export class AppModule {}
