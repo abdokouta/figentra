42de256c03975f9a4ec4a20094e6d1c52ee0276f
Pixiedia, Inc. <pixiedia@gmail.com>
2026-09-08 19:42:52 +0300

    fix(search): register OpenSearch adapter

--- services/search/src/app.module.ts
+++ services/search/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SearchController } from './modules/search/search.controller';
 import { SearchService } from './modules/search/search.service';
+import { SEARCH_PROVIDER } from './modules/search/search.port';
+import { OpenSearchSearchProvider } from './infrastructure/opensearch/opensearch-search.provider';
 
 @Module({
   controllers: [SearchController],
-  providers: [SearchService],
+  providers: [SearchService, { provide: SEARCH_PROVIDER, useClass: OpenSearchSearchProvider }],
 })
 export class AppModule {}
